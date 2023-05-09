b9fb05ec3d463e8675ec83982553d0a93cc1b34e
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-09 18:01:11 +0200

    3.2.1

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -1,38 +1,27 @@
 package com.bartosztobiasz;
 
-import java.text.NumberFormat;
-import java.util.Scanner;
-
-// ang. mortgage /morgydż/ -> pol. hipoteka, obciążyć hipotekę
-// https://www.wikihow.com/Calculate-Mortgage-Payments
-// Math.pow()
-// principal -> amount of loan (pol. pożyczka)
-// ang. interest rate -> pol. stopa procentowa
 public class Main {
     public static void main(String[] args) {
-        // avoid magic numbers in your code, always use constants (final variables) to describe them
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
+        int x = 1; // = -> assignment operator
+        int y = 1;
 
-        Scanner scanner = new Scanner(System.in);
+        // == -> equality operator
+        // x == y -> boolean expression
+        System.out.println(x == y);
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        // != -> inequality operator
+        System.out.println(x != y);
 
-        System.out.print("Annual Interest Rate [%]: ");
-        // do not use magic names for your variables, always use meaningful and descriptive names
-        float annualInterestRate = scanner.nextFloat();
-        float monthlyInterestRate = annualInterestRate / MONTHS_IN_YEAR / PERCENT;
+        // > -> greater than operator
+        System.out.println(x > y);
 
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
-        short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+        // < -> less than operator
+        System.out.println(x < y);
 
-        double mortgage = principal
-                * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments))
-                / (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
+        // >= -> greater than or equal to operator
+        System.out.println(x >= y);
 
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.print("Mortgage: " + mortgageFormatted);
+        // <= -> less than or equal to operator
+        System.out.println(x <= y);
     }
 }
