b345a9960954bc0ce80f10049fe3d6ac955fbf8f
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-09 15:09:01 +0200

    2.11.2

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,18 +2,8 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
-        int result;
-
-        result = 10 + 3; // addition
-        System.out.println(result);
-
-        result = 10 - 3; // subtraction
-        System.out.println(result);
-
-        result = 10 * 3; // multiplication
-        System.out.println(result);
-
-        result = 10 / 3; // division of two whole numbers in Java is a whole number
+        // An expression is a piece of code that produces a value
+        double result = (double) 10 / (double) 3; // casting two operands
         System.out.println(result);
     }
 }
