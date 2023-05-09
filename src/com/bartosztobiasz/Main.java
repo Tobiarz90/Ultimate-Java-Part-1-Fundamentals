7116999458f1098c2e71b387d451fb5b304042a5
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-09 16:15:45 +0200

    2.14.2

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,23 +2,15 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
+        double result_ = Math.random(); // from 0 to 1
+        System.out.println(result_);
+
         int result;
 
-        result = Math.round(1.1F);
+        result = (int) Math.round(Math.random() * 100);
         System.out.println(result);
 
-        // 1.1 => 2; zaokrąglenie w górę (sufit)
-        result = (int) Math.ceil(1.1F); // ang. ceiling  -> pol. sufit
-        System.out.println(result);
-
-        // 1.1 => 1; zaokrąglenie w dół (podłoga)
-        result = (int) Math.floor(1.1F); // ang. floor  -> pol. podłoga
-        System.out.println(result);
-
-        result = Math.max(1, 2); // which one is greater?
-        System.out.println(result);
-
-        result = Math.min(1, 2); // which one is smaller?
+        result = (int) (Math.random() * 100);
         System.out.println(result);
     }
 }
