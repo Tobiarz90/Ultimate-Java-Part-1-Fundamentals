13a728dc075be01710fedac547773a0d93be7f09
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-09 12:12:04 +0200

    2.3.1

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,8 +2,18 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
-        int myAge = 17;
-        int herAge = myAge; // copy value
-        System.out.println(herAge);
+//        int age = 30;
+        byte age = 30;
+
+//        int viewsCount = 123_456_789;
+        long viewsCount = 3_123_456_789L; // l/L
+
+//        double price = 10.99;
+        float price = 10.99f; // f/F
+
+        // single quote = single character
+        char letter = 'A';
+
+        boolean isEligible = false; // pol. spełniający wymagania/warunki
     }
 }
