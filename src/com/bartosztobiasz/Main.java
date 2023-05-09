abbe2ea9020d91b356a61cee2f0de917603f1dc9
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-09 16:23:15 +0200

    2.15.1

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -1,16 +1,17 @@
 package com.bartosztobiasz;
 
+import java.text.NumberFormat;
+
 public class Main {
     public static void main(String[] args) {
-        double result_ = Math.random(); // from 0 to 1
-        System.out.println(result_);
+        String result;
 
-        int result;
-
-        result = (int) Math.round(Math.random() * 100);
+        NumberFormat currency = NumberFormat.getCurrencyInstance(); // factory method
+        result = currency.format(123456.789);
         System.out.println(result);
 
-        result = (int) (Math.random() * 100);
+        // Refactor: Inline Variable
+        result = NumberFormat.getPercentInstance().format(0.1); // method chaining
         System.out.println(result);
     }
 }
