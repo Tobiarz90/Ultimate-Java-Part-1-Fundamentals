26c492eba109a275bc19718e76d9ca562cb09bf4
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-09 13:52:35 +0200

    2.7.1

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,39 +2,25 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
-        // "Hello World" is a string literal/value
-
-        // java.lang package is automatically imported
-
         /*
-        Unless an explicit copy of original is needed,
-        use of this constructor is unnecessary since Strings are immutable.
+        special characters:
+        " -> \"
+        \ -> \\
+        \n
+        \t
          */
-//        String message = new String("Hello World");
+        String message;
 
-//        String message = "Hello World"; // shorthand to initialize a string variable
-//        System.out.println(message);
+        message = "Hello \"Mosh\"";
+        System.out.println(message);
 
-        // join a string with another one -> concatenate two strings
-        String message = "Hello World" + "!!";
+        message = "c:\\Windows\\...";
+        System.out.println(message);
 
-        System.out.println(message.endsWith("!!"));
-        System.out.println(message.startsWith("!!"));
+        message = "c:\nWindows\\...";
+        System.out.println(message);
 
-        System.out.println(message.length());
-
-        System.out.println(message.indexOf("H"));
-        System.out.println(message.indexOf("sky")); // -1
-
-        System.out.println(message.replace("!", "*")); // ang. asterisk -> pol. gwiazdka
-        System.out.println(message); // String.replace() doesn't modify the original string
-        // Strings are immutable (pol. niezmienne)
-
-        System.out.println(message.toLowerCase());
-        System.out.println(message.toUpperCase());
-
-        message = "  Hello World" + "!!  ";
-        System.out.println(message.trim()); // all leading and trailing spaces are removed
+        message = "c:\tWindows\\...";
         System.out.println(message);
     }
 }
