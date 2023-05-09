3eb69b03af41373669fb87bb6e02af4cc947cdf8
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-09 12:06:02 +0200

    2.2.2

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,9 +2,8 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
-//        int age = 30, temperature = 20;
-        int age = 30; // type name/label/identifier assignment_operator initial_value
-        age = 35;
-        System.out.println(age);
+        int myAge = 17;
+        int herAge = myAge; // copy value
+        System.out.println(herAge);
     }
 }
