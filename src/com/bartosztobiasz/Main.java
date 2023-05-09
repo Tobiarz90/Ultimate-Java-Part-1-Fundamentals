ca1b991138a718fee47ecd6b0a84622f485d5668
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-09 15:06:35 +0200

    2.11.1

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,7 +2,18 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
-        final float PI = 3.14F; // constant / final variable
-//        PI = 1; // cannot change its value
+        int result;
+
+        result = 10 + 3; // addition
+        System.out.println(result);
+
+        result = 10 - 3; // subtraction
+        System.out.println(result);
+
+        result = 10 * 3; // multiplication
+        System.out.println(result);
+
+        result = 10 / 3; // division of two whole numbers in Java is a whole number
+        System.out.println(result);
     }
 }
