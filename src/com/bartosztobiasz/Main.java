694060acac18a0ff48f4d83b4995397568001ffc
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-09 20:39:11 +0200

    3.14.1

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -1,23 +1,56 @@
 package com.bartosztobiasz;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+// ang. mortgage /morgydż/ -> pol. hipoteka, obciążyć hipotekę
+// https://www.wikihow.com/Calculate-Mortgage-Payments
+// Math.pow()
+// principal -> amount of loan (pol. pożyczka)
+// ang. interest rate -> pol. stopa procentowa
 public class Main {
     public static void main(String[] args) {
-        // for each -> iterates over an array or a collections
+        // avoid magic numbers in your code, always use constants (final variables) to describe them
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
 
-        String[] fruits = {"Apple", "Mango", "Orange"};
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < fruits.length; i++) {
-            System.out.println(fruits[i]);
+        int principal;
+        while (true) {
+            System.out.print("Principal [$1K - $1M]: ");
+            principal = scanner.nextInt();
+            if (principal >= 1_000 && principal < 1_000_000)
+                break;
+            System.out.println("Enter number between 1,000 and 1,000,000");
         }
 
-        // for (loop variable : array or collection) { code block }
-        // for each loop is FORWARD only
-        for (String fruit : fruits) {
-            System.out.println(fruit);
+        float annualInterestRate;
+        while (true) {
+            System.out.print("Annual Interest Rate [%]: ");
+            // do not use magic names for your variables, always use meaningful and descriptive names
+            annualInterestRate = scanner.nextFloat();
+            if (annualInterestRate > 0 && annualInterestRate <= 30)
+                break;
+            System.out.println("Enter number between 1 and 30");
         }
+        float monthlyInterestRate = annualInterestRate / MONTHS_IN_YEAR / PERCENT;
 
-        for (int i = fruits.length - 1; i >= 0; i--) {
-            System.out.println(fruits[i]);
+        byte years;
+        while (true) {
+            System.out.print("Period (Years): ");
+            years = scanner.nextByte();
+            if (years > 0 && years <= 30)
+                break;
+            System.out.println("Enter number between 1 and 30");
         }
+        short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+
+        double mortgage = principal
+                * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments))
+                / (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.print("Mortgage: " + mortgageFormatted);
     }
 }
