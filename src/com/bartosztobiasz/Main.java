f0310cd304adb19b16426dd400a86a79cb6e1fa4
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-09 14:57:23 +0200

    2.9.1

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -4,25 +4,11 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        // Arrays are reference types
-        // Arrays have a fixed size/length
+//        int[][] numbers = new int[2][3];
+//        numbers[0][0] = 1; // array_name[row][column]
 
-        // older syntax
-//        int[] numbers = new int[5];
-//        numbers[0] = 1;
-//        numbers[1] = 2;
-//        numbers[10] = 3; // Exception here
+        int[][] numbers = {{1, 2, 3}, {4, 5, 6}};
 
-        // newer syntax
-        int[] numbers = {2, 3, 5, 1, 4};
-
-        System.out.println(numbers);
-
-        System.out.println(numbers.length);
-
-        Arrays.sort(numbers);
-
-        // Arrays.toString() is implemented multiple times => the method is overloaded (method overloading)
-        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.deepToString(numbers)); // printing multidimensional arrays
     }
 }
