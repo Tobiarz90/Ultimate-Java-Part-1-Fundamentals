960765c82c461e63fc4de52f9e38075142ab9a17
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-09 18:24:54 +0200

    3.3.1

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,26 +2,27 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 1; // = -> assignment operator
-        int y = 1;
+        /*
+        How to combine multiple boolean expressions?
+        Use logical operators.
+         */
+        // logical operators represent real world rules
 
-        // == -> equality operator
-        // x == y -> boolean expression
-        System.out.println(x == y);
+        // && -> AND operator; only if both conditions are true
+        int temperature = 22;
+        boolean isWarm = temperature > 20 && temperature < 30; // Java evaluates this expression from left to right
+        System.out.println(isWarm);
 
-        // != -> inequality operator
-        System.out.println(x != y);
+        boolean isEligible;
+        // || -> OR operator; if at least one condition is true
+        boolean hasHighIncome = false;
+        boolean hasGoodCredit = true;
+        isEligible = hasHighIncome || hasGoodCredit;
+        System.out.println(isEligible);
 
-        // > -> greater than operator
-        System.out.println(x > y);
-
-        // < -> less than operator
-        System.out.println(x < y);
-
-        // >= -> greater than or equal to operator
-        System.out.println(x >= y);
-
-        // <= -> less than or equal to operator
-        System.out.println(x <= y);
+        // ! -> NOT operator; reverses a value
+        boolean hasCriminalRecord = false;
+        isEligible = hasHighIncome || hasGoodCredit && !hasCriminalRecord;
+        System.out.println(isEligible);
     }
 }
