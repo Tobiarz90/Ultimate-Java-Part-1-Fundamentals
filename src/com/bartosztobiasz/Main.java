0e4a3ae64ef2ac0806b237f0669ba7103e5a5ed3
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-09 19:53:02 +0200

    3.10.1

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -1,21 +1,32 @@
 package com.bartosztobiasz;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // for loop -> when you know how many times you want to execute statement/-s
+        // while loop => when you DON'T know how many times you want to execute statement/-s
 
-        // i, j, k, ...
-        // for (loop/counter variable; loop condition -> boolean expression; i++) { code block }
-        for (int i = 0; i < 5; i++) {
+        int i = 1;
+        // while (loop condition -> boolean expression) { code block }
+        while (i <= 5) {
             System.out.println("Hello World " + i);
+            i++;
         }
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("Hello World " + i);
+        int j = 5;
+        while (j > 0) {
+            System.out.println("Hello World " + j);
+            j--;
         }
 
-        for (int i = 5; i > 0; i--) {
-            System.out.println("Hello World " + i);
+        Scanner scanner = new Scanner(System.in);
+
+        String input = "";
+        // comparison operators compare the address of a string object, not its value
+        while (!input.equals("quit")) {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
         }
     }
 }
