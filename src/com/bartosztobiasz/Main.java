6927e72ef5fb058ddb74b7b38ba3caf15156fe4c
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-09 15:53:34 +0200

    2.13.4

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,15 +2,12 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-         Aby operacja matematyczna została wykonana,
-         kompilator najpierw konwertuje elementy wyrażenia do wspólnego typu danych
-         */
-        // explicit (pol. jawny) casting, manual conversion/casting -> data loss
+        String x = "1";
 
-        double x = 1.1;
-        // (int) x => data loss here
-        int y = (int) x + 2; // 1 + 2
+        // every primitive type has a reference type wrapper
+        // Integer -> wrapper for int
+        int y = Integer.parseInt(x) + 2; // 1 + 2
+
         System.out.println(y);
     }
 }
