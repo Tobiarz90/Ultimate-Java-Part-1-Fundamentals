b76d952be57695a99d17f8f1179a777221cab779
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-09 18:53:56 +0200

    3.5.1

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,21 +2,26 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
-        // if statements allow making decisions based on certain conditions
-        int temp = 32;
+        int income = 120_000;
 
-        // if (boolean expression/value) { code block }
-        if (temp > 30) {
-            // first section/clause
-            System.out.println("It's a hot day");
-            System.out.println("Drink water");
-//        } else if (temp > 20 && temp <= 30) {
-        } else if (temp > 20) {
-            // second section/clause
-            System.out.println("Beautiful day");
-        } else {
-            // third section/clause
-            System.out.println("Cold day");
-        }
+        // #1
+//        boolean hasHighIncome;
+//        if (income > 100_000) {
+//            hasHighIncome = true;
+//        } else {
+//            hasHighIncome = false;
+//        }
+//        System.out.println(hasHighIncome);
+
+        // #2
+//        boolean hasHighIncome = false;
+//        if (income > 100_000) {
+//            hasHighIncome = true;
+//        }
+//        System.out.println(hasHighIncome);
+
+        // #3
+        boolean hasHighIncome = (income > 100_000);
+        System.out.println(hasHighIncome);
     }
 }
