1284cad7835be275de1acf38c7629a535f7cf3e0
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-10 17:10:24 +0200

    4.8.1

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -9,8 +9,14 @@ import java.util.Scanner;
 // L -> principal / amount of loan
 // c -> monthly interest
 // n -> total number of payments
-// p -> remaining number of payments
+// p -> number of payments made
 public class Main {
+    // avoid magic numbers in your code, always use constants (final variables) to describe them
+    // class level variable -> class field
+    // static methods can only access static variables
+    final static byte MONTHS_IN_YEAR = 12;
+    final static byte PERCENT = 100;
+
     // ideal method -> from 5 to 10 lines and no more than 20 lines
 
     /*
@@ -30,52 +36,15 @@ public class Main {
     5. invalid value => print error message
      */
     public static void main(String[] args) {
-        // avoid magic numbers in your code, always use constants (final variables) to describe them
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
-
         int principal = (int) readNumber("Principal ($1K - $1M): ", 1_000, 1_000_000);
         float annualInterestRate = (float) readNumber("Annual Interest Rate: ", 1, 30);
         byte years = (byte) readNumber("Period (Years): ", 1, 30);
 
-        short totalNumberOfPayments = (short) (years * MONTHS_IN_YEAR);
-        float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
-
-        printMortgage(principal, totalNumberOfPayments, monthlyInterestRate);
         System.out.println();
-        printPaymentSchedule(principal, totalNumberOfPayments, monthlyInterestRate);
-    }
+        printMortgage(principal, annualInterestRate, years);
 
-    private static void printMortgage(int principal, short totalNumberOfPayments, float monthlyInterestRate) {
-        double mortgage = calculateMortgage(principal, monthlyInterestRate, totalNumberOfPayments);
-
-        System.out.println("MORTGAGE");
-        System.out.println("--------");
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Monthly Payments: " + mortgageFormatted);
-    }
-
-    private static void printPaymentSchedule(int principal, short totalNumberOfPayments, float monthlyInterestRate) {
-        System.out.println("PAYMENT SCHEDULE");
-        System.out.println("----------------");
-
-        for (short remainingNumberOfPayments = 1; remainingNumberOfPayments <= totalNumberOfPayments; remainingNumberOfPayments++) {
-            double remainingBalance = calculateRemainingBalance(principal, monthlyInterestRate, totalNumberOfPayments, remainingNumberOfPayments);
-            String remainingBalanceFormatted = NumberFormat.getCurrencyInstance().format(remainingBalance);
-            System.out.println(remainingBalanceFormatted);
-        }
-    }
-
-    private static double calculateRemainingBalance(
-            int principal,
-            float monthlyInterestRate,
-            short totalNumberOfPayments,
-            short remainingNumberOfPayments) {
-
-        return principal
-                * (Math.pow(1 + monthlyInterestRate, totalNumberOfPayments)
-                    - Math.pow(1 + monthlyInterestRate, remainingNumberOfPayments))
-                / (Math.pow(1 + monthlyInterestRate, totalNumberOfPayments) - 1);
+        System.out.println();
+        printPaymentSchedule(principal, annualInterestRate, years);
     }
 
     // every question = different type of value => use double as a return type
@@ -94,11 +63,54 @@ public class Main {
         return value;
     }
 
+    private static void printMortgage(int principal, float annualInterestRate, byte years) {
+        double mortgage = calculateMortgage(principal, annualInterestRate, years);
+
+        System.out.println("MORTGAGE");
+        System.out.println("--------");
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Monthly Payments: " + mortgageFormatted);
+    }
+
+    private static void printPaymentSchedule(int principal, float annualInterestRate, byte years) {
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+
+        for (short numberOfPaymentsMade = 1; numberOfPaymentsMade <= years * MONTHS_IN_YEAR; numberOfPaymentsMade++) {
+            double remainingBalance = calculateRemainingBalance(principal, annualInterestRate, years, numberOfPaymentsMade);
+            String remainingBalanceFormatted = NumberFormat.getCurrencyInstance().format(remainingBalance);
+            System.out.println(remainingBalanceFormatted);
+        }
+    }
+
+    private static double calculateRemainingBalance(
+            int principal,
+            float annualInterestRate,
+            byte years,
+            short numberOfPaymentsMade) {
+
+        short totalNumberOfPayments = (short) (years * MONTHS_IN_YEAR);
+        float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
+
+        return principal
+                * (Math.pow(1 + monthlyInterestRate, totalNumberOfPayments)
+                - Math.pow(1 + monthlyInterestRate, numberOfPaymentsMade))
+                / (Math.pow(1 + monthlyInterestRate, totalNumberOfPayments) - 1);
+    }
+
     // logic for calculating mortgage in one place
     public static double calculateMortgage(
             int principal,
-            float monthlyInterestRate,
-            short numberOfPayments) {
+            float annualInterestRate,
+            byte years) {
+
+        /*
+        These two lines are duplicated once.
+        A better solution would be to move this logic to the main method,
+        but to remain this method reusable in other projects, you shouldn't use this solution.
+         */
+        short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+        float monthlyInterestRate = annualInterestRate / PERCENT / MONTHS_IN_YEAR;
 
         return principal
                 * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments))
