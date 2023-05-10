eed7645886cf26f0335a3575c55da0f6695a68c7
Bartosz Tobiasz <barticz90@gmail.com>
2023-05-10 15:05:30 +0200

    4.3.2

--- src/com/bartosztobiasz/Main.java
+++ src/com/bartosztobiasz/Main.java
@@ -2,16 +2,11 @@ package com.bartosztobiasz;
 
 public class Main {
     public static void main(String[] args) {
-        // methodName(arguments);
-        greetUser("Mosh", "Hamedani"); // method call
-        greetUser("Bartosz", "Tobiasz"); // methods are reusable
+        String message = greetUser("Mosh", "Hamedani");
+        System.out.println(message);
     }
 
-    // access_modifier static (?) return_type/void methodName(parameters (?)) { code block }
-    // (?) -> optional
-    // static -> this method belongs to a class, not an object
-    // parameters can be use like a local variables inside the method
-    public static void greetUser(String firstName, String lastName) {
-        System.out.println("Hello " + firstName + " " + lastName); // concatenating strings
+    public static String greetUser(String firstName, String lastName) {
+        return "Hello " + firstName + " " + lastName;
     }
 }
